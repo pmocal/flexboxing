282acbfe8a66e450c837955beec6c96e2e457a22
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-06-18 17:15:55 -0400

    added dropdown

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 document.getElementById("datetime").innerHTML = (new Date()).toLocaleDateString();
+document.getElementById("signup").addEventListener("click",
+	function(){
+		console.log(document.getElementById("dropdown"));
+		if (document.getElementById("dropdown").style.display === "block"){
+			document.getElementById("dropdown").style.display = "none";
+		} else {
+			document.getElementById("dropdown").style.display = "block";
+		}
+	}
+);
