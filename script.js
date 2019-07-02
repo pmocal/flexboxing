7ebd9a3a421a9f2eafc5e8c906d9154e5083cbbc
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-07-01 20:56:59 -0400

    created hamburger menu for mobile

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ document.getElementById("signup").addEventListener("click",
 		}
 	}
 );
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
